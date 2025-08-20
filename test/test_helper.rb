3984d9d60131dc64f3af1eb06ea78df673746e00
Gerjan Stokkink <gerjan@bookingexperts.com>
2025-08-20 16:54:14 +0200

    Fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'minitest/autorun'
-require 'mocha/mini_test'
-require 'pry'
+require 'mocha/minitest'
 require 'tileup'
 
 GEM_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
